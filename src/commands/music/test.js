02068fcf7eb1c6efb99a33c75377a9643d00d77b
Porya <theporyafm12@gmail.com>
2021-07-13 13:48:16 -0700

    Unnecessarily packages (ESLint & Mongoose) removed for now

---
+++ src/commands/music/test.js
@@ -0,0 +1,9 @@
+module.exports.run = async (client, message, args) => {
+    message.channel.send('Ok <:AMOGUS:848317305090867261>');
+}
+
+module.exports.config = {
+    name: 'test',
+    aliases: [],
+    description: 'Tests',
+};
